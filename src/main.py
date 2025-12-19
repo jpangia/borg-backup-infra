476824af5591c2d70d997ef4ba4ce35c088963bd
jpangia <jvpangia@gmail.com>
2025-12-18 23:29:47 -0500

    begin backup code

--- src/main.py
+++ src/main.py
@@ -1,37 +1,15 @@
-import borg
+from borg import Borg
 import util
 import config
-
-def backup_home():
-    """
-    Creates a backup of the home directory
-    """
-
-    borg.ensure_init()
-    pass
-
-def backup_more():
-    """
-    Creates a backup of the More drive
-    """
-    borg.ensure_init()
-    pass
-
-def backup_windows():
-    """
-    Creates a backup of the Windows drive
-    """
-    borg.ensure_init()
-    pass
 
 def main():
     """
     run everything.
     
     """
-    util.load_env()
     cfg = config.load_config_or_default()
+    util.load_env(cfg)
 
-    backup_home()
-    backup_more()
-    backup_windows()
+    bg = Borg(cfg)
+
+    bg.backup_all()
